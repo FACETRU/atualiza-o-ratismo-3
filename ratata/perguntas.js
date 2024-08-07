e69d776d31d5344bfa5f430ae97d7e49e96160eb
Nathan <79485616+FACETRU@users.noreply.github.com>
2024-08-07 11:42:10 -0300

    Add files via upload

---
+++ ratata/perguntas.js
@@ -0,0 +1,167 @@
+export const perguntas = [
+    {
+        enunciado: "Com o inicio dessa novo mundo comandado pelos ratos, como você vai agir nos primeiros momentos desse novo comando?",
+        alternativas: [
+            {
+                texto: "Amarei ao rato",
+                afirmacao: [
+                    "Os deuses ja de cara te amaram,",
+                    "Os deuses já te abençoaram de cara graças as suas atitudes,"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Amarei a ratoeira",
+                afirmacao: [
+                    "Os deus inicialmente já não foram muito com a sua cara"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Graças as suas boas atitudes os deuses te abençoaram com algumas novas habilidades, oque você fara com elas?",
+        alternativas: [
+            {
+                texto: "Usarei espalhar ainda mais a palavra dos ratismo.",
+                afirmacao: [
+                    "e graça a você continuar espalhando a palavra do ratismo, deuses continuaram abençoando sua vida durante muito tempo,,",
+                  
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Usarei para ir contra o ratismo e seguir o caminho do mal",
+                afirmacao: [
+                    "mas essa admiração não acabou durando muito pois graça as suas más atitudes os deuses ficaram com uma pulga atrás da orelha sobre sua pessoa."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Começaram a surgir muitas igrejas ratistas, lugar para obter conhecimento sobre a palavra dos ratos. Como você vai lidar com as igrejas?",
+        alternativas: [
+            {
+                texto: "Começarei a ir em todos os cultos e obterei conhecimento sobre a palvra dos ratos e me arrependirei dos meus pecados",
+                afirmacao: [
+                    "mas a partir do momento que você começou a frequentar a igraja, deus te ouviram e de coração aberto, desculparam seus pecados",
+                    
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Continuarei a pecar e seguir o caminho do mal",
+                afirmacao: [
+                    "e você confirmou a duvida que os deuses tinham sobre seu carater, fazendo com que te jogassem na profundeza do inferno",
+                    
+                ],
+                proxima: 5,
+            },
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
